b609e7e9ff89524bbe497fb17fde71f3ebe7fc15
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-06-23 16:53:20 -0400

    Update models.py

--- list/models.py
+++ list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Checklist(models.Model):
+    store_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
