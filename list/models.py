36b7b95e0f1276a1d6991c62ee63388ad8b92f5c
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-06-23 20:56:28 -0400

    Update models.py

--- list/models.py
+++ list/models.py
@@ -5,4 +5,8 @@ from django.db import models
 class Checklist(models.Model):
     store_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
+class item(models.Model):
+    item_desc = models.CharField(max_length=100)
+    item_vpn = models.CharField(max_length=10)
+    item_crc = models.CharField(max_length=7)
