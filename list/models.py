76304f0ddd47030bf31e0146b498fbcb9b1091e9
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-06-24 11:38:50 -0400

    Update models.py

--- list/models.py
+++ list/models.py
@@ -11,5 +11,12 @@ class item(models.Model):
     item_vpn = models.CharField(max_length=10)
     item_crc = models.CharField(max_length=7)
 
+    class is_displayed(models.IntegerChoices):
+        NO = 0, _('No')
+        YES = 1, _('Yes')
+
+        __empty__ = _('(Unknown)')
+
 class pop(models.Model):
     pop_desc = models.CharField(max_length=20)
+
