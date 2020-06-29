a234623266c40d1decd566c8e6311c8839d676b0
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-06-29 18:13:59 -0400

    database setup
    created database, need to edit api

--- list/models.py
+++ list/models.py
@@ -10,13 +10,13 @@ class item(models.Model):
     item_desc = models.CharField(max_length=100)
     item_vpn = models.CharField(max_length=10)
     item_crc = models.CharField(max_length=7)
-
+'''
     class is_displayed(models.IntegerChoices):
         NO = 0, _('No')
         YES = 1, _('Yes')
         
         __empty__ = _('(Unknown)')
-
+'''
 class pop(models.Model):
     pop_desc = models.CharField(max_length=20)
 
