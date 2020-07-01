8078c78fe7e8f43935ba59a7149d067a719cad16
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-07-01 19:29:37 -0400

    fixed pop and item tables

--- list/models.py
+++ list/models.py
@@ -8,6 +8,10 @@ class item(models.Model):
     item_desc = models.CharField(max_length=100)
     item_vpn = models.CharField(max_length=10)
     item_crc = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.item_desc
+
 '''
     class is_displayed(models.IntegerChoices):
         NO = 0, _('No')
@@ -18,8 +22,15 @@ class item(models.Model):
 class pop(models.Model):
     pop_desc = models.CharField(max_length=20)
 
+    def __str_(self):
+        return self.pop_desc
+
 class Checklist(models.Model):
     store_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    list_of_items = models.ForeignKey(item)
-    list_of_pop = models.ForeignKey(pop)
+    list_of_items = models.ForeignKey(
+        item,
+        on_delete=models.CASCADE)
+    list_of_pop = models.ForeignKey(
+        pop,
+        on_delete=models.CASCADE)
