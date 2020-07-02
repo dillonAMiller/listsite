c4f9cde9eeb5ef9285d4617bbcdb51e77f700b9b
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-07-02 12:14:12 -0400

    is displayed y/n selector

--- list/admin.py
+++ list/admin.py
@@ -7,3 +7,9 @@ from . import models
 admin.site.register(models.Checklist)
 admin.site.register(models.item)
 admin.site.register(models.pop)
+
+
+'''
+is_displayed object changed to attribute
+admin.site.register(models.item.is_displayed)
+'''
--- list/models.py
+++ list/models.py
@@ -8,17 +8,22 @@ class item(models.Model):
     item_desc = models.CharField(max_length=100)
     item_vpn = models.CharField(max_length=10)
     item_crc = models.CharField(max_length=7)
-
+    is_displayed_y_n = (
+        ('0', 'No'),
+        ('1', 'Yes'),
+    )
+    is_displayed = models.CharField(max_length=1, choices=is_displayed_y_n)
+    
     def __str__(self):
         return self.item_desc
 
-
-    class is_displayed(models.IntegerChoices):
+'''
+    class is_displayed(models.Model):
         NO = 0, ('No')
         YES = 1, ('Yes')
         
         __empty__ = ('(Unknown)')
-
+'''
 class pop(models.Model):
     pop_desc = models.CharField(max_length=20)
 
