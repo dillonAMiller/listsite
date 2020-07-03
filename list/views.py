7f150ed9f1a36ea34d26094c8d29155b84664eaf
dillonAMiller <64083498+dillonAMiller@users.noreply.github.com>
2020-07-03 16:22:50 -0400

    store list view

--- list/views.py
+++ list/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Checklist, item, pop, sets
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 # Create your views here.
  
 def index(request):
-    return HttpResponse("This is where the store levels will be")
-    
+    store_list = Checklist.objects.order_by('store_name')
+    output = ', '.join([q.store_name for q in store_list])
+    return HttpResponse(output)
+
